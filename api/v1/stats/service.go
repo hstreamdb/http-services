74810b0ecc8449e8966ae24ac7b57520d8367793
alissa-tung <alissa-tung@outlook.com>
2022-06-27 08:19:23 +0800

    wip

--- api/v1/stats/service.go
+++ api/v1/stats/service.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/hstreamdb/hstreamdb-go/hstream"
 	"github.com/hstreamdb/http-server/api/model"
+	"github.com/hstreamdb/http-server/pkg/errorno"
 	"net/http"
 	"strconv"
 )
@@ -54,47 +55,43 @@ func (s *Service) GetServerAppendLatency(c *gin.Context) {
 	aggAvg(s, c, "server stats histogram append_latency")
 }
 
-// FIXME
-func getFromCluster(s *Service, c *gin.Context, cmd string) []model.TableResult {
+func adminError(err error) errorno.ErrorResponse {
+	return errorno.NewErrorResponse(errorno.ADMIN_GET_STATUS_ERROR, err)
+}
+
+func getFromCluster(s *Service, cmd string) ([]model.TableResult, error) {
 	info, err := s.client.GetServerInfo()
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusInternalServerError, fmt.Errorf("%v", err))
-		return nil
+		return nil, fmt.Errorf("GetServerInfo error: %v", err)
 	}
 
 	allStatsRaw := make([]model.TableResult, 0)
 
 	for _, x := range info {
-		func() {
+		err := func() error {
 			client, err := hstream.NewHStreamClient(x)
 			if err != nil {
-				c.AbortWithStatusJSON(http.StatusInternalServerError, fmt.Errorf("%v", err))
-				return
+				return fmt.Errorf("NewHStreamClient error with %v: %v", x, err)
 			}
 			defer client.Close()
 
 			resp, err := client.AdminRequest(cmd)
 			if err != nil {
-				c.AbortWithStatusJSON(http.StatusInternalServerError, fmt.Errorf("%v", err))
-				return
+				return fmt.Errorf("AdminRequest error with %v: %v", cmd, err)
 			}
 
 			var jsonObj map[string]json.RawMessage
 			if err := json.Unmarshal([]byte(resp), &jsonObj); err != nil {
-				c.AbortWithStatusJSON(http.StatusInternalServerError, fmt.Errorf("%v", err))
-				return
+				return fmt.Errorf("unmarshal to RawMessage error: %v", err)
 			}
 
 			var table model.TableType
 			if content, ok := jsonObj["content"]; ok {
 				if err = json.Unmarshal(content, &table); err != nil {
-					c.AbortWithStatusJSON(http.StatusInternalServerError, fmt.Errorf("%v", err))
-					return
+					return fmt.Errorf("unmarshal to TableType error: %v", err)
 				}
 			} else {
-				// FIXME
-				c.AbortWithStatusJSON(http.StatusInternalServerError, fmt.Errorf("%v", err))
-				return
+				return fmt.Errorf("JSON object does not have the `content` field")
 			}
 
 			stats := model.TableResult{
@@ -111,18 +108,20 @@ func getFromCluster(s *Service, c *gin.Context, cmd string) []model.TableResult 
 			}
 
 			allStatsRaw = append(allStatsRaw, stats)
+			return nil
 		}()
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	return allStatsRaw
+	return allStatsRaw, nil
 }
 
 func aggAppendSum(s *Service, c *gin.Context, cmd string) {
-
-	allStatsRaw := getFromCluster(s, c, cmd)
-	if allStatsRaw == nil {
-		// FIXME
-		c.AbortWithStatusJSON(http.StatusInternalServerError, fmt.Errorf("%v", nil))
+	allStatsRaw, err := getFromCluster(s, cmd)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, adminError(err))
 		return
 	}
 
@@ -136,46 +135,71 @@ func aggAppendSum(s *Service, c *gin.Context, cmd string) {
 
 		for _, stats := range allStatsRaw {
 			for _, val := range stats.Value {
-				// FIXME
-				allStats.Value = append(allStats.Value, val)
+				mainKey := val[headers[0]]
+
+				ok := false
+				ix := -1
+				for i, cur := range allStats.Value {
+					if cur[mainKey] == val[mainKey] {
+						ok = true
+						ix = i
+					}
+				}
+
+				if ok {
+					for k := range allStats.Value[ix] {
+						allStats.Value[ix][k] += val[k]
+					}
+				} else {
+					allStats.Value = append(allStats.Value, val)
+				}
 			}
 		}
 
 		c.JSON(http.StatusOK, allStats)
 	} else {
-		// FIXME
-		c.AbortWithStatusJSON(http.StatusInternalServerError, fmt.Errorf("%v", nil))
+		c.AbortWithStatusJSON(http.StatusInternalServerError, adminError(fmt.Errorf("got stats of length 0")))
 		return
 	}
 }
 
 func aggAvg(s *Service, c *gin.Context, cmd string) {
-	allStatsRaw := getFromCluster(s, c, cmd)
-	if allStatsRaw == nil {
-		// FIXME
-		c.AbortWithStatusJSON(http.StatusInternalServerError, fmt.Errorf("%v", nil))
+	allStatsRaw, err := getFromCluster(s, cmd)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, adminError(err))
 		return
 	}
 
 	if len(allStatsRaw) != 0 || len(allStatsRaw[0].Value) != 1 {
 		headerLen := len(allStatsRaw[0].Headers)
-		acc := make([]float64, headerLen, 0.0)
+		acc := make([]float64, headerLen)
 		for _, stats := range allStatsRaw {
 			for _, val := range stats.Value {
 				for i, k := range stats.Headers {
 					val, err := strconv.ParseFloat(val[k], 64)
 					if err != nil {
-						c.AbortWithStatusJSON(http.StatusInternalServerError, fmt.Errorf("%v", err))
+						c.AbortWithStatusJSON(http.StatusInternalServerError, adminError(fmt.Errorf("%v", err)))
 						return
 					}
 					acc[i] += val
 				}
 			}
 		}
+		l := len(allStatsRaw)
+		for i := range acc {
+			acc[i] /= float64(l)
+		}
+
+		c.JSON(http.StatusOK, nil)
 
 	} else {
-		// FIXME
-		c.AbortWithStatusJSON(http.StatusInternalServerError, fmt.Errorf("%v", nil))
+		var err errorno.ErrorResponse
+		if len(allStatsRaw) != 0 {
+			err = adminError(fmt.Errorf("got stats of length 0"))
+		} else if len(allStatsRaw[0].Value) != 1 {
+			err = adminError(fmt.Errorf("the length value should be 1"))
+		}
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
 		return
 	}
 }
